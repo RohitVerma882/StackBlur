3e09a533768d0389f7493cf6ea23bc4889d39c90
RohitVermaOP <rohitkumar882333@gmail.com>
2022-03-01 09:10:37 +0530

    removed unused import

--- app/src/main/java/com/itsrohit/stackblursample/StackBlurApp.java
+++ app/src/main/java/com/itsrohit/stackblursample/StackBlurApp.java
@@ -2,7 +2,6 @@ package com.itsrohit.stackblursample;
 
 import android.app.Application;
 import cat.ereza.customactivityoncrash.config.CaocConfig;
-import com.itsrohit.stackblur.StackBlur;
 
 public class StackBlurApp extends Application {
 
